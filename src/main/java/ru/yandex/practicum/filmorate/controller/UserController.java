0fd3ef69ac0b50776087ed1a469a4561905f5677
User Podsekina <margaritapodsekina@gmail.com>
2024-07-01 00:02:39 +0300

    Добавила ветку

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,55 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Integer, User> users = new HashMap<>();
+
+    @GetMapping
+    public ArrayList<User> getAll() {
+        return new ArrayList<>(users.values());
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        log.info("==>POST /users {}", user);
+        user.setId(getNextId());
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        users.put(user.getId(), user);
+        log.info("POST /user <== {}", user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@Valid @RequestBody User user) {
+        log.info("==>PUT /users {}", user);
+        if (users.get(user.getId()) == null) {
+            throw new NotFoundException("Юзер с id:" + user.getId() + "не найден");
+        }
+        users.put(user.getId(), user);
+        log.info("PUT /users <== {}", user);
+        return user;
+    }
+
+    private Integer getNextId() {
+        int currentMaxId = users.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,38 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
 public class Film {
+    private int id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Size(max = 200)
+    private String description;
+    private LocalDate releaseDate;
+
+    @Positive
+    private int duration;
+
+    @AssertTrue
+    private boolean isValidReleaseDate() {
+        return releaseDate.isAfter(LocalDate.of(1895, Month.DECEMBER, 28));
+    }
+
+
 }
