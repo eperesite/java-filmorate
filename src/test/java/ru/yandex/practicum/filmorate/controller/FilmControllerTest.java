de640b62905db302a30680f6f11b457d7c81dcf6
User Podsekina <margaritapodsekina@gmail.com>
2024-09-02 00:21:47 +0300

    11ТЗ

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,91 +1,130 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
+import java.time.LocalDate;
 import java.util.Collections;
-import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(FilmController.class)
-class FilmControllerTest {
+public class FilmControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
+    @Mock
     private FilmService filmService;
 
-    private Film film;
+    @InjectMocks
+    private FilmController filmController;
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(filmController).build();
+        objectMapper = new ObjectMapper();
+    }
 
     @Test
-    void shouldCreateFilm() throws Exception {
+    void testGetAll() throws Exception {
+        Film film = new Film(1L, "Film Title", "Film Description", LocalDate.of(2022, 1, 1), 120);
+        when(filmService.getAll()).thenReturn(Collections.singletonList(film));
+
+        mockMvc.perform(get("/films"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id").value(film.getId()))
+                .andExpect(jsonPath("$[0].name").value(film.getName()))
+                .andExpect(jsonPath("$[0].description").value(film.getDescription()))
+                .andExpect(jsonPath("$[0].releaseDate").value(film.getReleaseDate().toString()))
+                .andExpect(jsonPath("$[0].duration").value(film.getDuration()));
+    }
+
+    @Test
+    void testCreate() throws Exception {
+        Film film = new Film(1L, "Film Title", "Film Description", LocalDate.of(2022, 1, 1), 120);
         when(filmService.save(any(Film.class))).thenReturn(film);
 
         mockMvc.perform(post("/films")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(film)))
                 .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id").value(film.getId()))
-                .andExpect(jsonPath("$.name").value(film.getName()));
+                .andExpect(jsonPath("$.name").value(film.getName()))
+                .andExpect(jsonPath("$.description").value(film.getDescription()))
+                .andExpect(jsonPath("$.releaseDate").value(film.getReleaseDate().toString()))
+                .andExpect(jsonPath("$.duration").value(film.getDuration()));
     }
 
     @Test
-    void shouldUpdateFilm() throws Exception {
+    void testUpdate() throws Exception {
+        Film film = new Film(1L, "Updated Film Title", "Updated Film Description", LocalDate.of(2022, 1, 1), 130);
         when(filmService.update(any(Film.class))).thenReturn(film);
 
         mockMvc.perform(put("/films")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(film)))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id").value(film.getId()))
-                .andExpect(jsonPath("$.name").value(film.getName()));
+                .andExpect(jsonPath("$.name").value(film.getName()))
+                .andExpect(jsonPath("$.description").value(film.getDescription()))
+                .andExpect(jsonPath("$.releaseDate").value(film.getReleaseDate().toString()))
+                .andExpect(jsonPath("$.duration").value(film.getDuration()));
     }
 
     @Test
-    void shouldReturnAllFilms() throws Exception {
-        when(filmService.getAll()).thenReturn(List.of(film));
+    void testLike() throws Exception {
+        doNothing().when(filmService).addLike(1L, 2L);
 
-        mockMvc.perform(get("/films"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").value(film.getId()))
-                .andExpect(jsonPath("$[0].name").value(film.getName()));
-    }
-
-    @Test
-    void shouldAddLike() throws Exception {
         mockMvc.perform(put("/films/1/like/2"))
                 .andExpect(status().isOk());
+
         verify(filmService, times(1)).addLike(1L, 2L);
     }
 
     @Test
-    void shouldDeleteLike() throws Exception {
+    void testDeleteLike() throws Exception {
+        doNothing().when(filmService).deleteLike(1L, 2L);
+
         mockMvc.perform(delete("/films/1/like/2"))
                 .andExpect(status().isOk());
+
         verify(filmService, times(1)).deleteLike(1L, 2L);
     }
 
     @Test
-    void shouldReturnPopularFilms() throws Exception {
+    void testGetPopular() throws Exception {
+        Film film = new Film(1L, "Popular Film", "Popular Film Description", LocalDate.of(2022, 1, 1), 120);
         when(filmService.getPopular(10)).thenReturn(Collections.singletonList(film));
 
-        mockMvc.perform(get("/films/popular"))
+        mockMvc.perform(get("/films/popular")
+                        .param("count", "10"))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$[0].id").value(film.getId()))
-                .andExpect(jsonPath("$[0].name").value(film.getName()));
+                .andExpect(jsonPath("$[0].name").value(film.getName()))
+                .andExpect(jsonPath("$[0].description").value(film.getDescription()))
+                .andExpect(jsonPath("$[0].releaseDate").value(film.getReleaseDate().toString()))
+                .andExpect(jsonPath("$[0].duration").value(film.getDuration()));
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,100 +1,144 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.util.Collections;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
-class UserControllerTest {
+public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
+    @Mock
     private UserService userService;
 
-    private User user;
+    @InjectMocks
+    private UserController userController;
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+        objectMapper = new ObjectMapper();
+    }
 
     @Test
-    void shouldCreateUser() throws Exception {
+    void testGetAll() throws Exception {
+        User user = new User(1L, "user@example.com", "username", "User Name", LocalDate.of(1990, 1, 1));
+        when(userService.getAll()).thenReturn(Collections.singletonList(user));
+
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id").value(user.getId()))
+                .andExpect(jsonPath("$[0].email").value(user.getEmail()))
+                .andExpect(jsonPath("$[0].login").value(user.getLogin()))
+                .andExpect(jsonPath("$[0].name").value(user.getName()))
+                .andExpect(jsonPath("$[0].birthday").value(user.getBirthday().toString()));
+    }
+
+    @Test
+    void testCreate() throws Exception {
+        User user = new User(1L, "user@example.com", "username", "User Name", LocalDate.of(1990, 1, 1));
         when(userService.save(any(User.class))).thenReturn(user);
 
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id").value(user.getId()))
-                .andExpect(jsonPath("$.name").value(user.getName()));
+                .andExpect(jsonPath("$.email").value(user.getEmail()))
+                .andExpect(jsonPath("$.login").value(user.getLogin()))
+                .andExpect(jsonPath("$.name").value(user.getName()))
+                .andExpect(jsonPath("$.birthday").value(user.getBirthday().toString()));
     }
 
     @Test
-    void shouldUpdateUser() throws Exception {
+    void testUpdate() throws Exception {
+        User user = new User(1L, "user@example.com", "username", "User Name", LocalDate.of(1990, 1, 1));
         when(userService.update(any(User.class))).thenReturn(user);
 
         mockMvc.perform(put("/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(user)))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id").value(user.getId()))
-                .andExpect(jsonPath("$.name").value(user.getName()));
+                .andExpect(jsonPath("$.email").value(user.getEmail()))
+                .andExpect(jsonPath("$.login").value(user.getLogin()))
+                .andExpect(jsonPath("$.name").value(user.getName()))
+                .andExpect(jsonPath("$.birthday").value(user.getBirthday().toString()));
     }
 
     @Test
-    void shouldReturnAllUsers() throws Exception {
-        when(userService.getAll()).thenReturn(List.of(user));
+    void testAddFriend() throws Exception {
+        doNothing().when(userService).addFriend(1L, 2L);
 
-        mockMvc.perform(get("/users"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").value(user.getId()))
-                .andExpect(jsonPath("$[0].name").value(user.getName()));
-    }
-
-    @Test
-    void shouldAddFriend() throws Exception {
         mockMvc.perform(put("/users/1/friends/2"))
                 .andExpect(status().isOk());
+
         verify(userService, times(1)).addFriend(1L, 2L);
     }
 
     @Test
-    void shouldDeleteFriend() throws Exception {
+    void testDeleteFriend() throws Exception {
+        doNothing().when(userService).deleteFriend(1L, 2L);
+
         mockMvc.perform(delete("/users/1/friends/2"))
                 .andExpect(status().isOk());
+
         verify(userService, times(1)).deleteFriend(1L, 2L);
     }
 
     @Test
-    void shouldReturnFriends() throws Exception {
-        when(userService.getFriends(1L)).thenReturn(List.of(user));
+    void testGetFriends() throws Exception {
+        User user = new User(1L, "friend@example.com", "friendname", "Friend Name", LocalDate.of(1990, 1, 1));
+        when(userService.getFriends(1L)).thenReturn(Collections.singletonList(user));
 
         mockMvc.perform(get("/users/1/friends"))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$[0].id").value(user.getId()))
-                .andExpect(jsonPath("$[0].name").value(user.getName()));
+                .andExpect(jsonPath("$[0].email").value(user.getEmail()))
+                .andExpect(jsonPath("$[0].login").value(user.getLogin()))
+                .andExpect(jsonPath("$[0].name").value(user.getName()))
+                .andExpect(jsonPath("$[0].birthday").value(user.getBirthday().toString()));
     }
 
     @Test
-    void shouldReturnCommonFriends() throws Exception {
-        when(userService.getCommonFriends(1L, 2L)).thenReturn(List.of(user));
+    void testGetCommonFriends() throws Exception {
+        User user = new User(2L, "commonfriend@example.com", "commonfriend", "Common Friend", LocalDate.of(1990, 1, 1));
+        when(userService.getCommonFriends(1L, 2L)).thenReturn(Collections.singletonList(user));
 
         mockMvc.perform(get("/users/1/friends/common/2"))
                 .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$[0].id").value(user.getId()))
-                .andExpect(jsonPath("$[0].name").value(user.getName()));
+                .andExpect(jsonPath("$[0].email").value(user.getEmail()))
+                .andExpect(jsonPath("$[0].login").value(user.getLogin()))
+                .andExpect(jsonPath("$[0].name").value(user.getName()))
+                .andExpect(jsonPath("$[0].birthday").value(user.getBirthday().toString()));
     }
 }
